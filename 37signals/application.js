fae8eda5ac7f112750894319b6c5ad03c17eddf9
admin <admin@phongmacbook.gateway.2wire.net>
2014-04-04 00:16:48 -0700

    no message

---
+++ 37signals/application.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$("#logo").after("<h1 id="popup1">Basecamp is the project management tool you wish you had on your last project.</h1><h2>Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborte with our team and clients the modern way.<h2>");
+});
